7dea38659ae107e9d3eeea2c13f1cd64902e0398
Pablo Bustos <pbustos@unex.es>
2018-10-10 15:02:49 +0200

    moving rect

---
+++ chocachoca/src/specificworker.cpp
@@ -0,0 +1,82 @@
+/*
+ *    Copyright (C)2018 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
+{
+
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+//       THE FOLLOWING IS JUST AN EXAMPLE
+//
+//	try
+//	{
+//		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+//		innermodel_path = par.value;
+//		innermodel = new InnerModel(innermodel_path);
+//	}
+//	catch(std::exception e) { qFatal("Error reading config params"); }
+
+	
+	// Scene
+	scene.setSceneRect(-2500, -2500, 5000, 5000);
+	view.setScene(&scene);
+	view.scale(1, -1);
+	//view.setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
+	robot = scene.addRect(QRectF(-200, -200, 400, 400));
+
+	
+	timer.start(Period);
+	return true;
+}
+
+void SpecificWorker::compute()
+{
+	static RoboCompGenericBase::TBaseState bState;
+ 	try
+ 	{
+ 		differentialrobot_proxy->getBaseState(bState);
+	}
+ 	catch(const Ice::Exception &e)
+	{	std::cout << "Error reading from Camera" << e << std::endl; }
+	
+	robot->moveBy((int)(10.*std::rand()/RAND_MAX),(int)(10.*std::rand()/RAND_MAX));
+	draw();
+}
+
+
+void SpecificWorker::draw()
+{
+	view.show();
+}
+
+
