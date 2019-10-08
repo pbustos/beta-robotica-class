14d4793a01055129feecefe5d1d1e1dd70022d50
Pablo Bustos <pbustos@unex.es>
2019-10-08 19:43:10 +0200

    chocamem

---
+++ chocamem/src/specificworker.cpp
@@ -0,0 +1,141 @@
+/*
+ *    Copyright (C)2019 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
+{}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+	
+	RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+	innerModel = std::make_shared<InnerModel>(par.value);
+	xmin = std::stoi(params.at("xmin").value);
+	xmax = std::stoi(params.at("xmax").value);
+	ymin = std::stoi(params.at("ymin").value);
+	ymax = std::stoi(params.at("ymax").value);
+	tilesize = std::stoi(params.at("tilesize").value);
+
+	// Scene
+ 	scene.setSceneRect(xmin, ymin, fabs(xmin)+fabs(xmax), fabs(ymin)+fabs(ymax));
+ 	view.setScene(&scene);
+ 	view.scale(1, -1);
+ 	view.setParent(scrollArea);
+ 	view.fitInView(scene.sceneRect(), Qt::KeepAspectRatio );
+	grid.initialize( TDim{ tilesize, xmin, xmax, ymin, ymax}, TCell{true, false, nullptr} );
+
+	qDebug() << "Grid initialize ok";
+
+	for(auto &[key, value] : grid)
+ 	{
+	 	value.rect = scene.addRect(-tilesize/2,-tilesize/2, 100,100, QPen(Qt::NoPen));			
+		value.rect->setPos(key.x,key.z);
+	}
+
+ 	robot = scene.addRect(QRectF(-200, -200, 400, 400), QPen(), QBrush(Qt::blue));
+ 	noserobot = new QGraphicsEllipseItem(-50,100, 100,100, robot);
+ 	noserobot->setBrush(Qt::magenta);
+
+	view.show();
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+
+	this->Period = period;
+	timer.start(Period);
+	qDebug() << "End initialize";
+
+}
+
+void SpecificWorker::compute()
+{
+	readRobotState();
+
+	/// AQUI LA MAQUINA DE ESTADOS
+}
+	
+
+void SpecificWorker::readRobotState()
+{
+	try
+	{
+		differentialrobot_proxy->getBaseState(bState);
+		innerModel->updateTransformValues("base", bState.x, 0, bState.z, 0, bState.alpha, 0);
+		RoboCompLaser::TLaserData ldata = laser_proxy->getLaserData();
+		
+		//draw robot
+		robot->setPos(bState.x, bState.z);
+		robot->setRotation(-180.*bState.alpha/M_PI);
+
+		//update  occupied cells
+		updateOccupiedCells(bState, ldata);
+	}
+	catch(const Ice::Exception &e)
+	{
+		std::cout << "Error reading from Laser" << e << std::endl;
+	}
+	//Resize world widget if necessary, and render the world
+	if (view.size() != scrollArea->size())
+	 		view.setFixedSize(scrollArea->width(), scrollArea->height());
+	
+}
+
+void SpecificWorker::updateOccupiedCells(const RoboCompGenericBase::TBaseState &bState, const RoboCompLaser::TLaserData &ldata)
+{
+	InnerModelLaser *n = innerModel->getNode<InnerModelLaser>(QString("laser"));
+	for(auto l: ldata)
+	{
+		auto r = n->laserTo(QString("world"), l.dist, l.angle);	// r is in world reference system
+		// we set the cell corresponding to r as occupied 
+		auto [valid, cell] = grid.getCell(r.x(), r.z()); 
+		if(valid)
+		{
+			cell.free = false;
+			cell.rect->setBrush(Qt::darkRed);
+		}
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////77
+////  SUBSCRIPTION
+/////////////////////////////////////////////////////////////////////
+
+void SpecificWorker::RCISMousePicker_setPick(const Pick &myPick)
+{
+//subscribesToCODE
+
+}
+
+
+
---
+++ chocamem/src/specificworker.h
@@ -0,0 +1,85 @@
+/*
+ *    Copyright (C)2019 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+       \brief
+       @author authorname
+*/
+
+
+
+#ifndef SPECIFICWORKER_H
+#define SPECIFICWORKER_H
+
+#include <iostream>
+#include <fstream>
+#include <genericworker.h>
+#include <innermodel/innermodel.h>
+#include "grid.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsEllipseItem>
+
+class SpecificWorker : public GenericWorker
+{
+Q_OBJECT
+public:
+	SpecificWorker(MapPrx& mprx);
+	~SpecificWorker();
+	bool setParams(RoboCompCommonBehavior::ParameterList params);
+	void RCISMousePicker_setPick(const Pick &myPick);
+
+
+	/// Grid cell definition
+		struct TCell
+		{
+			bool free;
+			bool visited;
+			QGraphicsRectItem* rect;
+			
+			// method to save the value
+			void save(std::ostream &os) const {	os << free << " " << visited; };
+			void read(std::istream &is) {	is >> free >> visited ;};
+		};
+
+public slots:
+	void compute();
+	void initialize(int period);
+
+private:
+	std::shared_ptr<InnerModel> innerModel;
+	using TDim = Grid<TCell>::Dimensions;
+	Grid<TCell> grid;
+	QGraphicsScene scene;
+	QGraphicsView view;
+	void draw();
+	QGraphicsRectItem *robot;
+	QGraphicsEllipseItem *noserobot;
+	
+	int tilesize = 70;
+	int xmin, xmax, ymin, ymax;
+
+	RoboCompGenericBase::TBaseState bState;
+	RoboCompLaser::TLaserData ldata;
+
+	void updateOccupiedCells(const RoboCompGenericBase::TBaseState &bState, const RoboCompLaser::TLaserData &ldata);
+	void readRobotState();
+};
+
+#endif
